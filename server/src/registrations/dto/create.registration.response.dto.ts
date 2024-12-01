8da8022e2a95c538be18e1c76a16ea86352bc09f
Mohamed072005 <amine072005@gmail.com>
2024-12-01 20:11:59 +0100

    GBBA-4 create the readme file

--- server/src/registrations/dto/create.registration.response.dto.ts
+++ server/src/registrations/dto/create.registration.response.dto.ts
@@ -1,7 +1,22 @@
-import { Registration } from "../registration.schema"
+import { ApiProperty } from '@nestjs/swagger';
+import { Registration } from "../registration.schema";
 
-export interface CreateRegistrationResponseDTO {
-    message: string
-    registration: Registration
-    statusCode: number
+export class CreateRegistrationResponseDTO {
+    @ApiProperty({ 
+        description: 'Descriptive message about the registration',
+        example: 'Registration created successfully' 
+    })
+    message: string;
+
+    @ApiProperty({ 
+        description: 'Details of the created registration',
+        type: () => Registration 
+    })
+    registration: Registration;
+
+    @ApiProperty({ 
+        description: 'HTTP status code of the response',
+        example: 201 
+    })
+    statusCode: number;
 }
--- server/src/registrations/dto/get.registrations.response.dto.ts
+++ server/src/registrations/dto/get.registrations.response.dto.ts
@@ -1,6 +1,17 @@
-import { Registration } from "../registration.schema"
+import { ApiProperty } from '@nestjs/swagger';
+import { Registration } from "../registration.schema";
 
-export interface GetRegistrationsResponseDTO {
-    statusCode: number
-    registrations: Registration[]
+export class GetRegistrationsResponseDTO {
+    @ApiProperty({ 
+        description: 'HTTP status code of the response',
+        example: 202 
+    })
+    statusCode: number;
+
+    @ApiProperty({ 
+        description: 'List of registrations',
+        type: () => Registration,
+        isArray: true 
+    })
+    registrations: Registration[];
 }
